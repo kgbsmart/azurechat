794ea8a6dddcd124544ef1fb5ca529d74dd14fc4
kgbsmart <keiji.baba@smart-group.co.jp>
2024-11-15 11:09:33 +0900

    Update main-menu.tsx

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -43,11 +43,15 @@ export const MainMenu = async () => {
               <VenetianMask {...menuIconProps} />
             </MenuLink>
           </MenuItem>
-          <MenuItem tooltip="extensions">
-            <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
-              <PocketKnife {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
+          {user.isAdmin && (
+            <>
+              <MenuItem tooltip="extensions">
+                <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
+                  <PocketKnife {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+            </>
+          )}
           <MenuItem tooltip="prompts">
             <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
               <Book {...menuIconProps} />
